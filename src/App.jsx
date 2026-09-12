12709ccb167749f4b14816922f833bd2a9832a9a
shruti <khandareshruti5@gmail.com>
2026-09-12 21:42:20 +0530

    My portfolio website

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { div } from 'framer-motion/client'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Navbar from './components/Navbar'
+
+const App = () => {
+   return (
+       <div>
+        <Navbar />
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+        </Routes>
+        
+       </div>
+   )
+}
+
+export default App
